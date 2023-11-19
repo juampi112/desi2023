69afd2a724977ca4b1eb27ab3b299946bbdfb14b
Nicolas Ceschi <nicolasc@vertiq.com>
2023-11-19 19:00:07 -0300

    finish first part

--- src/main/java/tuti/desi/servicios/VueloService.java
+++ src/main/java/tuti/desi/servicios/VueloService.java
@@ -1,5 +1,8 @@
 package tuti.desi.servicios;
 
+import java.sql.Date;
+
+import tuti.desi.entidades.Avion;
 import tuti.desi.entidades.Vuelo;
 import tuti.desi.excepciones.Excepcion;
 /**
@@ -10,8 +13,10 @@ import tuti.desi.excepciones.Excepcion;
 
 public interface VueloService {
 
-	
-
 	Vuelo save(Vuelo v) throws Excepcion;
+
+	boolean exist(String numVuelo) throws Excepcion;
 	
+	boolean existVueloRepetido(Avion avion,Date fechayHora) throws Excepcion;
+		
 }
